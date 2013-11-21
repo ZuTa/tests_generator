cb7ea78505b09b96d7a02ab99931978e885c5e36
ZuTa <zuta.developer@gmail.com>
2013-11-21 16:54:41 +0200

    Removed duplicates

--- runners/csharprunner.py
+++
@@ -1,7 +0,0 @@
-from baserunner import BaseRunner
-
-class CSharpRunner(BaseRunner):
-    def create_run_command(self, path_to_source, path_to_input_file):
-        execution_file = path_to_source[:path_to_source.rfind('.')] + '.exe'
-
-        return 'mcs -warn:0 {} && mono {} < {}'.format(path_to_source, execution_file, path_to_input_file)
--- runners/pythonrunner.py
+++
@@ -1,5 +0,0 @@
-from baserunner import BaseRunner
-
-class PythonRunner(BaseRunner):
-    def create_run_command(self, path_to_source, path_to_input_file):
-        return 'python {} < {}'.format(path_to_source, path_to_input_file)
