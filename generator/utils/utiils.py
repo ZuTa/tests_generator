ce8fe56225fb6eee209968aca0f8ae5331dfd8a8
ZuTa <zuta.developer@gmail.com>
2014-03-05 17:23:09 +0200

    Add Reservoir Sampling function [utils]

--- generator/utils/utiils.py
+++ generator/utils/utiils.py
@@ -2,7 +2,30 @@ from random import randint
 
 
 def shuffle(collection):
+    """Shuffle the collection"""
+
     for i in xrange(1, len(collection)):
         j = randint(0, i)
         if i != j:
             collection[i], collection[j] = collection[j], collection[i]
+
+
+def reservoir_sampling(collection, k):
+    """Returns k randomly selected elements from the given collection.
+    k has to be positive and greater than 0.
+    """
+    n = len(collection)
+
+    if k < 0:
+        raise Exception('Incorrect input (k = %d)' % k)
+
+    if k > n:
+        raise Exception('Collection has only %d element(s)' % n)
+
+    result = collection[:k]
+    for i in xrange(k, n):
+        j = randint(0, i)
+        if j < k:
+            result[j] = collection[i]
+
+    return result
